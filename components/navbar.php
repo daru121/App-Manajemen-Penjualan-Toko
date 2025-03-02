3df5169a8e6add49afc2ab6f26d3966eab93cf92
daru121 <darucaraka43@gmail.com>
2025-03-02 21:08:06 +0800

    commit

--- components/navbar.php
+++ components/navbar.php
@@ -331,13 +331,13 @@ require_once '../backend/database.php';
                                     Pengaturan
                                 </a>
 
-                                <a href="tentang" class="flex items-center gap-3 px-3 py-2 text-sm text-gray-600 hover:bg-gray-50 rounded-xl transition-colors duration-200">
+                                <!-- <a href="tentang" class="flex items-center gap-3 px-3 py-2 text-sm text-gray-600 hover:bg-gray-50 rounded-xl transition-colors duration-200">
                                     <svg class="w-5 h-5 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
                                     </svg>
                                     Tentang Saya
                                 </a>
-                            </div>
+                            </div> -->
 
                             <div class="border-t border-gray-100"></div>
 
